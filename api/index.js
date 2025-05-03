aecd05bcac5c4cc989b347e21deaa5c601cf2015
Rishi2564 <161162489+Rishi2564@users.noreply.github.com>
2025-05-03 12:45:52 +0530

    connection made up between backend and database

--- api/index.js
+++ api/index.js
@@ -1,21 +1,34 @@
+require("dotenv").config();
 const express = require("express");
-const cors=require("cors");
-const  mongoose  = require("mongoose");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
+
 app.use(express.json());
 
-//BIGRjafO5bPhOTmm
-//mongodb+srv://rishabhpradhan2005:BIGRjafO5bPhOTmm@cluster0.a4fk3dg.mongodb.net/
-app.use(cors({
-    credentials:true,
-    origin:"http://localhost:5173",
-}));
-mongoose.connect('mongodb+srv://rishabhpradhan2005:BIGRjafO5bPhOTmm@cluster0.a4fk3dg.mongodb.net/')
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+async function connectMongo(){
+  try{
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to mongodb");
+  }
+  catch(error){
+    console.log(error);
+    process.exit(1);
+  }
+}
+connectMongo();
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
-app.post("/register",(req,res)=>{
-    const{name,email,password}= req.body;
-    res.json({name,email,password});
-})
-app.listen(4000);
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  res.json({ name, email, password });
+});
